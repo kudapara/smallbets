bb871e05e5c42b5fde2bf655ba2b39f6111873b4
Ira Zayats <ira@irazayats.com>
2024-02-21 17:21:43 +0200

    Reboosting 🚀

--- app/javascript/controllers/boost_delete_controller.js
+++ app/javascript/controllers/boost_delete_controller.js
@@ -3,7 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static classes = [ "reveal", "perform" ]
 
-  reveal({ params: { boosterId } }) {
+  reveal({ srcElement, params: { boosterId } }) {
+    // Can't rely on regular event.stopPropagation as it's needed for Turbo
+    if (srcElement.closest('[data-stop-propagation]')) return;
+    
     if (Current.user.id === boosterId) {
       this.element.classList.toggle(this.revealClass)
     }
