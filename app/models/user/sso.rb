7333c40abc545c1900d4e23cfcef0fb557b2290e
Ira Zayats <ira@irazayats.com>
2024-03-31 00:11:44 +0200

    Do not check SSO token expiration

--- app/models/user/sso.rb
+++ app/models/user/sso.rb
@@ -10,7 +10,7 @@ module User::Sso
   end
   
   def refresh_from_sso
-    return if sso_token.blank? || sso_token_expired?
+    return if sso_token.blank?
 
     sso_attributes = Sso::Fetch.new(token: sso_token).fetch(read_timeout: 1, open_timeout: 2)
     update(sso_attributes)
