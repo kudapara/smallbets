01475164f6891fbc7e2c4e318a1f108e4955849c
Ira Zayats <ira@irazayats.com>
2024-04-01 13:17:48 +0300

    Do not show expired thread in user profile

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -3,7 +3,7 @@ class Users::ProfilesController < ApplicationController
 
   def show
     @direct_memberships, @shared_memberships =
-      Current.user.memberships.with_ordered_room.partition { |m| m.room.direct? }
+      Current.user.memberships.without_expired_threads.with_ordered_room.partition { |m| m.room.direct? }
   end
 
   def update
