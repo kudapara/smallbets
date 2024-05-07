62f159807b410956a03f1817a1417992434910d8
Ira Zayats <ira@irazayats.com>
2024-05-07 15:50:49 +0300

    Allow bots to mention users

--- app/controllers/messages/by_bots_controller.rb
+++ app/controllers/messages/by_bots_controller.rb
@@ -11,10 +11,27 @@ class Messages::ByBotsController < MessagesController
       if params[:attachment]
         params.permit(:attachment)
       else
-        reading(request.body) { |body| { body: body } }
+        reading(request.body) { |body| { body: format_mentions(body) } }
+      end
+    end
+  
+    def format_mentions(body)
+      body.to_s.gsub(/@\{(.+?)\}/) do |mention_sig|
+        sso_user_id = $1
+        user = User.find_by(sso_user_id: sso_user_id)
+        if user
+          mention_user(user)
+        else
+          mention_sig
+        end
       end
     end
 
+    def mention_user(user)
+      attachment_body = render partial: "users/mention", locals: { user: user }
+      "<action-text-attachment sgid=\"#{user.attachable_sgid}\" content-type=\"application/vnd.campfire.mention\" content=\"#{attachment_body.gsub('"', '&quot;')}\"></action-text-attachment>"
+    end
+  
     def reading(io)
       io.rewind
       yield io.read.force_encoding("UTF-8")
