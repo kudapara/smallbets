5fb208b702379b18abef12fcafaae790f554a19a
Daniel Vassallo <daniel@hey.com>
2024-09-30 03:23:00 -0700

    Stop refreshing from SSO

--- app/models/user/sso.rb
+++ app/models/user/sso.rb
@@ -8,7 +8,7 @@ module User::Sso
   def refresh_from_sso
     return if sso_token.blank?
 
-    sso_attributes = Sso::Fetch.new(token: sso_token).fetch(read_timeout: 1, open_timeout: 2)
-    update(sso_attributes.except(:name, :email, :twitter_url, :linkedin_url, :personal_url))
+    # sso_attributes = Sso::Fetch.new(token: sso_token).fetch(read_timeout: 1, open_timeout: 2)
+    # update(sso_attributes.except(:name, :email, :twitter_url, :linkedin_url, :personal_url))
   end
 end
