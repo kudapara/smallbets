ff96dd5eeb4dec03a38491557748c28ce399a1fd
Ira Zayats <ira@irazayats.com>
2024-11-23 15:44:02 +0200

    Prevent multiple accounts with the same email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: :show
   before_action :verify_join_code, only: %i[ new create ]
+  before_action :start_otp_if_user_exists, only: :create
 
   def new
     @user = User.new
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
     start_new_session_for @user
     redirect_to root_url
   rescue ActiveRecord::RecordNotUnique
-    redirect_to new_session_url(email_address: user_params[:email_address])
+    start_otp_if_user_exists
   end
 
   def show
@@ -26,6 +27,22 @@ class UsersController < ApplicationController
 
     def verify_join_code
       head :not_found if Current.account.join_code != params[:join_code]
+    end
+  
+    def start_otp_if_user_exists
+      user = User.find_by(email_address: user_params[:email_address])
+
+      if user.present?
+        start_otp_for user
+        redirect_to new_auth_tokens_validations_path
+      end
+    end
+
+    def start_otp_for(user)
+      session[:otp_email_address] = user.email_address
+
+      auth_token = user.auth_tokens.create!(expires_at: 15.minutes.from_now)
+      auth_token.deliver_later
     end
 
     def user_params
