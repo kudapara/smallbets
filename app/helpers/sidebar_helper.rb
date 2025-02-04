11c668b60fd227db655fa3f1c5f565d205f6f2ee
Ira Zayats <ira@irazayats.com>
2025-02-04 21:29:56 +0200

    Sort sidebar inbox by most recently updated first

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -1,6 +1,6 @@
 module SidebarHelper
   def inbox_sort_order
-    sort_by_last_updated_oldest_first
+    sort_by_last_updated_newest_first
   end
 
   def starred_rooms_sort_order
@@ -25,13 +25,13 @@ module SidebarHelper
   def sort_by_most_active
     raw "data-sorted-list-attribute-value='size' data-sorted-list-attribute-type-value='number' data-sorted-list-order-value='desc'"
   end
-  
+
   def sort_by_last_updated_oldest_first
-    raw "data-sorted-list-attribute-value='updatedAt' data-sorted-list-attribute-type-value='number'"
+    raw "data-sorted-list-attribute-value='updatedAt'"
   end
 
   def sort_by_last_updated_newest_first
-    raw "data-sorted-list-attribute-value='updatedAt' data-sorted-list-attribute-type-value='number' data-sorted-list-order-value='desc'"
+    raw "data-sorted-list-attribute-value='updatedAt' data-sorted-list-order-value='desc'"
   end
 
   def sort_alphabetically
