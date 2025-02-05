9ba68835bd2c20c39ecb2a1dbf1558dac7dd7ed1
Ira Zayats <ira@irazayats.com>
2025-02-05 23:09:23 +0200

    Fixed a cache bug for threaded rooms in the sidebar + more speed improvements

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -37,6 +37,16 @@ module SidebarHelper
   def sort_alphabetically
     raw "data-sorted-list-attribute-value='name'"
   end
+  
+  def cache_sidebar_room(section, membership, thread_memberships)
+    if thread_memberships.any? { |m| m.room.parent_room&.id == membership.room_id }
+      yield # do not cache memberships with threads - little upside
+    else
+      cache(sidebar_membership_cache_key(section, membership)) do
+        yield
+      end
+    end
+  end
 
   def sidebar_membership_cache_key(prefix, membership)
     [ prefix, membership.room, membership.involvement, membership.unread?, membership.has_unread_notifications? ]
