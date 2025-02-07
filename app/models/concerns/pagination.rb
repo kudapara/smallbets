9ef0ebfc1653c095444a3bfacf69948a7cff3cdf
Ira Zayats <ira@irazayats.com>
2025-02-07 19:22:16 +0200

    Load a smaller page of messages when opening the room at last unread timestamp

--- app/models/concerns/pagination.rb
+++ app/models/concerns/pagination.rb
@@ -4,14 +4,14 @@ module Pagination
   PAGE_SIZE = 40
 
   included do
-    scope :last_page, -> { ordered.last(PAGE_SIZE) }
-    scope :first_page, -> { ordered.first(PAGE_SIZE) }
+    scope :last_page, ->(page_size = PAGE_SIZE) { ordered.last(page_size) }
+    scope :first_page, ->(page_size = PAGE_SIZE) { ordered.first(page_size) }
 
     scope :before, ->(record) { where("#{arel_table.name}.created_at < ?", record.created_at) }
     scope :after, ->(record) { where("#{arel_table.name}.created_at > ?", record.created_at) }
 
-    scope :page_before, ->(record) { before(record).last_page }
-    scope :page_after, ->(record) { after(record).first_page }
+    scope :page_before, ->(record, page_size=PAGE_SIZE) { before(record).last_page(page_size) }
+    scope :page_after, ->(record, page_size=PAGE_SIZE) { after(record).first_page(page_size) }
 
     scope :page_created_since, ->(time) { where("#{arel_table.name}.created_at > ?", time).first_page }
     scope :page_updated_since, ->(time) { where("#{arel_table.name}.updated_at > ?", time).last_page }
@@ -19,7 +19,7 @@ module Pagination
 
   class_methods do
     def page_around(record)
-      page_before(record) + [ record ] + page_after(record)
+      page_before(record, 10) + [ record ] + page_after(record, 20)
     end
 
     def paged?
