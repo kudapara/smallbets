be174b6fc49a2f8041ed47ae628c4bdc3abff733
Daniel Vassallo <daniel@hey.com>
2025-03-05 11:08:28 -0800

    Limit sidebar ping avatars to unread pings and pings updated in the past 7 days, and remove placeholder avatars

--- app/controllers/users/sidebars_controller.rb
+++ app/controllers/users/sidebars_controller.rb
@@ -1,36 +1,28 @@
 class Users::SidebarsController < ApplicationController
-  DIRECT_PLACEHOLDERS = 10
-
   def show
     memberships           = Current.user.memberships.visible.without_thread_rooms.with_has_unread_notifications.includes(:room)
-    @direct_memberships   = extract_direct_memberships(memberships)
-    other_memberships     = memberships.without(@direct_memberships)
+    
+    # Get all direct memberships and filter them
+    all_direct_memberships = memberships.select { |m| m.room.direct? }
+    @direct_memberships   = filter_direct_memberships(all_direct_memberships)
+    
+    # Get other memberships using the without_direct_rooms scope
+    other_memberships     = Current.user.memberships.visible.without_thread_rooms.without_direct_rooms.with_has_unread_notifications.includes(:room)
     @all_memberships      = other_memberships.with_room_by_last_active_newest_first
     @starred_memberships  = other_memberships.with_room_by_last_active_newest_first
 
     @direct_memberships.select! { |m| m.room.messages_count > 0 }
-    @direct_placeholder_users = find_direct_placeholder_users
   end
 
   private
-    def extract_direct_memberships(all_memberships)
-      all_memberships.select { |m| m.room.direct? }.sort_by { |m| m.room.updated_at }.reverse
-    end
-
-    def find_direct_placeholder_users
-      exclude_user_ids = Membership.active
-                         .joins("JOIN rooms ON memberships.room_id = rooms.id")
-                         .where(rooms: { type: "Rooms::Direct" })
-                         .where("rooms.id IN (SELECT room_id FROM memberships WHERE user_id = ? AND active = TRUE)", Current.user.id)
-                         .pluck(:user_id)
-                         .uniq
-                         .push(Current.user.id)
-      
-      User.active.where.not(id: exclude_user_ids).order(:created_at).limit([ DIRECT_PLACEHOLDERS - exclude_user_ids.count, 0 ].max)
-    end
-
-    # This method is no longer used, but kept for reference
-    def user_ids_already_in_direct_rooms_with_current_user
-      Membership.active.where(room_id: Current.user.rooms.directs.pluck(:id)).pluck(:user_id).uniq
+    def filter_direct_memberships(direct_memberships)
+      # Filter direct memberships to only include:
+      # 1. Memberships with unread messages
+      # 2. Memberships updated in the last 7 days
+      direct_memberships.select do |membership|
+        membership.unread? || 
+        membership.has_unread_notifications? || 
+        (membership.room.updated_at.present? && membership.room.updated_at >= 7.days.ago)
+      end.sort_by { |m| m.room.updated_at || Time.at(0) }.reverse
     end
 end
