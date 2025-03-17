4f652941698ab3176f25e6c62c9a85a08a4bcae6
Ira Zayats <ira@irazayats.com>
2025-03-17 14:08:53 +0200

    Debugging the notifier schedule

--- config/initializers/rufus_scheduler.rb
+++ config/initializers/rufus_scheduler.rb
@@ -3,10 +3,10 @@ require "rufus-scheduler"
 Rails.application.config.after_initialize do
   if Rails.env.production? && defined?(Rails::Server) && !defined?($rufus_scheduler)
     Rails.logger.info "Starting Rufus scheduler."
-    $rufus_scheduler = Rufus::Scheduler.new(tz: "America/Los_Angeles")
+    $rufus_scheduler = Rufus::Scheduler.new
 
     # Run at 1:00 AM and 1:00 PM every day.
-    $rufus_scheduler.cron "0 1,4,5,13 * * *" do
+    $rufus_scheduler.cron "0 1,6,7,13 * * * America/Los_Angeles" do
       UnreadMentionsNotifierJob.new.perform
     end
   end
