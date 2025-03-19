688bd571c47bf7768afb6e2b1ff586a6b360cf8b
Ira Zayats <ira@irazayats.com>
2025-03-19 14:48:16 +0200

    Trim mentions if more than 10 and link to the inbox view

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -6,7 +6,9 @@ class NotifierMailer < ApplicationMailer
 
   def unread_mentions(user, messages)
     @user = user
-    @messages = messages
+    total_message_count = messages.count
+    @messages = total_message_count > 10 ? messages.first(8) : messages
+    @more_mentions_count = [total_message_count - @messages.count, 0].max
 
     mail(to: @user.email_address, subject: "You have #{pluralize(messages.size, "unread mention")}")
   end
