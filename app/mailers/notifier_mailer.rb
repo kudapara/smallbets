1db156dac900b361eacd016e40c00da0b8236a80
Ira Zayats <ira@irazayats.com>
2025-03-19 15:02:41 +0200

    Minor improvements to mentions email

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -4,11 +4,19 @@ class NotifierMailer < ApplicationMailer
 
   helper_method :room_display_name
 
+  MAX_MENTIONS_COUNT = 10
+  MIN_MENTIONS_TO_WRAP = 3
+
   def unread_mentions(user, messages)
     @user = user
-    total_message_count = messages.count
-    @messages = total_message_count > 10 ? messages.first(8) : messages
-    @more_mentions_count = [total_message_count - @messages.count, 0].max
+    @total_messages_count = messages.count
+    messages_to_include_count = if @total_messages_count > MAX_MENTIONS_COUNT
+                                  [@total_messages_count - MIN_MENTIONS_TO_WRAP, MAX_MENTIONS_COUNT ].min
+                                else
+                                  @total_messages_count
+                                end
+    @messages = messages.first(messages_to_include_count)
+    @more_mentions_count = [@total_messages_count - messages_to_include_count, 0].max
 
     mail(to: @user.email_address, subject: "You have #{pluralize(messages.size, "unread mention")}")
   end
