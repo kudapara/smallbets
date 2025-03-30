183e280d875351b5410f489573126af676334bed
Daniel Vassallo <daniel@hey.com>
2025-03-29 21:47:26 -0700

    More resident expert tweaks

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -2,8 +2,7 @@ class ExpertsController < ApplicationController
   before_action :ensure_administrator!
 
   def show
-    expert_ids = [228, 1, 2844, 211, 70, 187, 1196, 333, 391, 39, 343, 697]
-    @expert_users = User.where(id: expert_ids).index_by(&:id)
+    @expert_users = User.where(id: Expert.user_ids).index_by(&:id)
     render layout: "application"
   end
 
