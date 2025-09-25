kudapara/smallbets

commit b92017edcae42dcc94edee169178150498923ab4

Avatar load speeds improved (fix for mentions within Trix editor)

app/views/autocompletable/users/_user.json.jbuilder
@@ -1,4 +1,4 @@
 json.name       h(user.name)
 json.value      user.id
-json.avatar_url fresh_user_avatar_url(user)
+json.avatar_url user.avatar_url.presence || fresh_user_avatar_url(user)
 json.sgid       user.attachable_sgid

commit f54e95a36f9b2900a286d5ab6ccb5f911a487aaa

Fix avatars in mentions (#45)
This commit
https://github.com/antiwork/smallbets/commit/b92017edcae42dcc94edee169178150498923ab4
introduced a bug - if a user has an `avatar_url` set, it takes
precedence over the uploaded file in mentions. This should be the other
way around.

Fixes https://github.com/antiwork/smallbets/issues/44

app/views/autocompletable/users/_user.json.jbuilder
@@ -3,5 +3,5 @@ json.ascii_name             user.ascii_name
 json.twitter_username       user.twitter_username
 json.linkedin_username      user.linkedin_username
 json.value                  user.id
-json.avatar_url             user.avatar_url.presence || user_image_path(user)
+json.avatar_url             user_image_path(user)
 json.sgid                   user.attachable_sgid